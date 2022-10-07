4fe5e61a4951267d99e3d32ad5595c896fe5cca5
Jainil Mayani <jainil.mayani11@gmail.com>
2022-10-08 02:38:16 +0530

    done 1.1

--- app/src/main/java/com/example/schoolattendanceapp/SheetActivity.java
+++ app/src/main/java/com/example/schoolattendanceapp/SheetActivity.java
@@ -11,8 +11,9 @@ import android.widget.TextView;
 
 import java.util.Calendar;
 
-public class SheetActivity extends AppCompatActivity {
+import javax.xml.transform.sax.SAXResult;
 
+public class SheetActivity extends AppCompatActivity {
 
 
     @Override
@@ -24,8 +25,9 @@ public class SheetActivity extends AppCompatActivity {
 
     }
 
+
     private void showTable() {
-        DbHelper dbHelper =  new DbHelper(this);
+        DbHelper dbHelper = new DbHelper(this);
         TableLayout tableLayout = findViewById(R.id.table_layout);
         long[] idArray = getIntent().getLongArrayExtra("idArray");
         int[] rollArray = getIntent().getIntArrayExtra("rollArray");
@@ -35,28 +37,31 @@ public class SheetActivity extends AppCompatActivity {
         int DAY_IN_MONTH = getDayInMonth(month);
 
         //row setup
-        int rowSize = idArray.length+1;
+        int rowSize = idArray.length + 1;
         TableRow[] rows = new TableRow[rowSize];
         TextView[] rolls_tvs = new TextView[rowSize];
         TextView[] name_tvs = new TextView[rowSize];
         TextView[][] status_tvs = new TextView[rowSize][DAY_IN_MONTH + 1];
 
         for (int i = 0; i < rowSize; i++) {
+
             rolls_tvs[i] = new TextView(this);
             name_tvs[i] = new TextView(this);
-            for (int j = 0; j < DAY_IN_MONTH; j++){
+
+            for (int j = 1; j <= DAY_IN_MONTH; j++) {
                 status_tvs[i][j] = new TextView(this);
             }
         }
 
         //header
         rolls_tvs[0].setText("Roll");
-        rolls_tvs[0].setTypeface(rolls_tvs[0].getTypeface(), Typeface.BOLD);
+        rolls_tvs[0].setTypeface(rolls_tvs[0].getTypeface(),Typeface.BOLD);
+
         name_tvs[0].setText("Name");
-        name_tvs[0].setTypeface(name_tvs[0].getTypeface(), Typeface.BOLD);
-        for (int i = 0; i <= DAY_IN_MONTH; i++) {
+        name_tvs[0].setTypeface(name_tvs[0].getTypeface(),Typeface.BOLD);
+        for (int i = 1; i <= DAY_IN_MONTH; i++) {
             status_tvs[0][i].setText(String.valueOf(i));
-            status_tvs[0][i].setTypeface(status_tvs[0][i].getTypeface(), Typeface.BOLD);
+            status_tvs[0][i].setTypeface(status_tvs[0][i].getTypeface(),Typeface.BOLD);
         }
 
         for (int i = 1; i < rowSize; i++) {
@@ -75,25 +80,26 @@ public class SheetActivity extends AppCompatActivity {
 
         for (int i = 0; i < rowSize; i++) {
             rows[i] = new TableRow(this);
-
             if (i%2==0)
                 rows[i].setBackgroundColor(Color.parseColor("#EEEEEE"));
             else rows[i].setBackgroundColor(Color.parseColor("#E4E4E4"));
 
+
             rolls_tvs[i].setPadding(16,16,16,16);
             name_tvs[i].setPadding(16,16,16,16);
-
             rows[i].addView(rolls_tvs[i]);
             rows[i].addView(name_tvs[i]);
 
             for (int j = 1; j <= DAY_IN_MONTH; j++) {
                 status_tvs[i][j].setPadding(16,16,16,16);
+
                 rows[i].addView(status_tvs[i][j]);
             }
 
             tableLayout.addView(rows[i]);
         }
         tableLayout.setShowDividers(TableLayout.SHOW_DIVIDER_MIDDLE);
+
 
     }
 
